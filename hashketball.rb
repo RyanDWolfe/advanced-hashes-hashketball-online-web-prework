acff8dfff9e8d8590bb6b1348a082b6f2fe5a619
Ryan Wolfe <ryan@studiohopfitness.com>
2019-02-05 19:53:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,33 @@
-# Write your code here!
+def game_hash
+
+hashketball = {
+:home => {
+  :team_name => " ",
+  :colors => [""],
+  :players => {
+    :number => " ",
+    :shoe => " ",
+    :points => " ",
+    :rebounds => " ",
+    :assists => " ",
+    :steals => " ",
+    :blocks => " ",
+    :slam_dunks => " "
+    }
+  
+}
+
+
+, :away
+
+
+  
+}
+
+
+
+
+end
 
 
 
