ee8746b5acaa49f816ac54cf5db30b909a109b40
Ryan Wolfe <ryan@studiohopfitness.com>
2019-02-05 19:58:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,32 +2,35 @@ def game_hash
 
 hashketball = {
 :home => {
-  :team_name => " ",
-  :colors => [""],
+  :team_name => "Brooklyn Nets",
+  :colors => ["Black", "White"],
   :players => {
-    :number => " ",
-    :shoe => " ",
-    :points => " ",
-    :rebounds => " ",
-    :assists => " ",
-    :steals => " ",
-    :blocks => " ",
-    :slam_dunks => " "
+    "Alan Anderson" => {
+      :number => " ",
+      :shoe => " ",
+      :points => " ",
+      :rebounds => " ",
+      :assists => " ",
+      :steals => " ",
+      :blocks => " ",
+      :slam_dunks => " "
+      }
     }
   },
 :away => {
   :team_name => " ",
   :colors => [""],
   :players => {
-    :number => " ",
-    :shoe => " ",
-    :points => " ",
-    :rebounds => " ",
-    :assists => " ",
-    :steals => " ",
-    :blocks => " ",
-    :slam_dunks => " "
-    }
+    "Alan Anderson" => {
+      :number => " ",
+      :shoe => " ",
+      :points => " ",
+      :rebounds => " ",
+      :assists => " ",
+      :steals => " ",
+      :blocks => " ",
+      :slam_dunks => " "
+      }
   }
 }
 
